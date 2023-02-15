4eb995a99f9edc2891ed2c802e6561bffedb5478
UGO <ugo@UGOs-MBP.lan>
2023-02-15 00:55:56 +0000

    Add post functionality

--- controllers/post.js
+++ controllers/post.js
@@ -1,10 +1,10 @@
 import { db } from "../connect.js"
 import jwt from "jsonwebtoken";
 import * as dotenv from "dotenv";
+import moment from "moment/moment.js";
 dotenv.config();
 
 export const getPosts = (req, res) => {
-    console.log(req)
     const token = req.cookies.acessToken;
     if (!token) return res.status(401).json("Not logged in!")
     jwt.verify(token, process.env.TOKEN_SECRET_KEY, (err, userInfo) => {
@@ -18,6 +18,28 @@ export const getPosts = (req, res) => {
         db.query(q, [userInfo.id, userInfo.id], (err, data) => {
             if (err) return res.status(500).json(err);
             return res.status(200).json(data);
+        });
+    });
+};
+
+export const addPost = (req, res) => {
+    const token = req.cookies.acessToken;
+    if (!token) return res.status(401).json("Not logged in!")
+    jwt.verify(token, process.env.TOKEN_SECRET_KEY, (err, userInfo) => {
+        if (err) return res.status(403).json("Token is not valid!")
+
+        const q = "INSERT INTO posts (`desc`, `img`, `createdAt`, `userId`) VALUES (?)";
+
+        const values = [
+            req.body.desc,
+            req.body.img,
+            moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+            userInfo.id
+        ]
+
+        db.query(q, [values], (err, data) => {
+            if (err) return res.status(500).json(err);
+            return res.status(200).json("Post has been created!");
         });
     });
 };
