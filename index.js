c96ef8d345b1695bef3a4ff85e71b443c7eab235
UGO <ugo@UGOs-MBP.lan>
2023-02-27 00:04:33 +0000

    Add multer to handle file upload

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import commentRoutes from "./routes/comments.js"
 import likeRoutes from "./routes/likes.js"
 import cors from "cors"
 import cookieParser from "cookie-parser";
+import multer from "multer";
 
 //Middlewares
 app.use((req,res,next) => {
@@ -18,6 +19,22 @@ app.use(cors({
     origin: "http://localhost:5173",
 }))
 app.use(cookieParser())
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "../client/public/upload")
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + file.originalname)
+    }
+  })
+  
+const upload = multer({ storage: storage })
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    const file = req.file;
+    res.status(200).json(file.filename)
+})
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
